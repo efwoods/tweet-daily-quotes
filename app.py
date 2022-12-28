887e06f1cd6fd17d796f3e12c23659322e33284d
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-12-28 23:15:17 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+import requests, random
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return parse_fav_quote()
+    # return render_template("index.html", title="Hello")
+
+def parse_fav_quote():
+    url = "https://efwoods.github.io/EvanWoodsFavoriteQuotes/quotesTwitterDB.json"
+    fav_quote = requests.request("GET", url).json()
+    quote = random.randint(0, len(fav_quote["quotes"]))
+    return fav_quote["quotes"][quote]
